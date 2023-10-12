46b040d2e80ac793344eccd04bf1827296536e67
collins.mbathi <collins.mbathi@stream4.tech>
2023-10-12 11:33:23 +0530

    Fix:fixed headname showing in options

--- dist/components/Form/fields/password.js
+++ dist/components/Form/fields/password.js
@@ -4,14 +4,22 @@ require("core-js/modules/es.symbol.description.js");
 require("core-js/modules/es.error.cause.js");
 require("core-js/modules/es.array.push.js");
 require("core-js/modules/es.object.assign.js");
+require("core-js/modules/es.weak-map.js");
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _react = _interopRequireDefault(require("react"));
+require("core-js/modules/web.dom-collections.iterator.js");
+var _react = _interopRequireWildcard(require("react"));
 var _string = _interopRequireDefault(require("./string"));
+var _IconButton = _interopRequireDefault(require("@mui/material/IconButton"));
+var _InputAdornment = _interopRequireDefault(require("@mui/material/InputAdornment"));
+var _Visibility = _interopRequireDefault(require("@mui/icons-material/Visibility"));
+var _VisibilityOff = _interopRequireDefault(require("@mui/icons-material/VisibilityOff"));
 const _excluded = ["otherProps"];
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
 function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
 function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
@@ -20,16 +28,31 @@ function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return typ
 function _toPrimitive(input, hint) { if (typeof input !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (typeof res !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
 function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-const field = _ref => {
+const Field = _ref => {
   let {
       otherProps
     } = _ref,
     props = _objectWithoutProperties(_ref, _excluded);
+  const [showPassword, setShowPassword] = (0, _react.useState)(false);
+  const handlePasswordToggle = () => {
+    setShowPassword(prevShowPassword => !prevShowPassword);
+  };
   otherProps = _objectSpread({
-    type: 'password'
+    type: showPassword ? "text" : 'password',
+    InputProps: {
+      endAdornment: /*#__PURE__*/_react.default.createElement(_InputAdornment.default, {
+        position: "end"
+      }, /*#__PURE__*/_react.default.createElement(_IconButton.default, {
+        onClick: handlePasswordToggle,
+        edge: "end",
+        sx: {
+          color: 'primary.periwinkle'
+        }
+      }, showPassword ? /*#__PURE__*/_react.default.createElement(_VisibilityOff.default, null) : /*#__PURE__*/_react.default.createElement(_Visibility.default, null)))
+    }
   }, otherProps);
   return /*#__PURE__*/_react.default.createElement(_string.default, _extends({
     otherProps: otherProps
   }, props));
 };
-var _default = exports.default = field;
+var _default = exports.default = Field;
--- src/lib/components/Form/fields/password.js
+++ src/lib/components/Form/fields/password.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 import StringField from './string';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
-const field = ({ otherProps, ...props }) => {
+const Field = ({ otherProps, ...props }) => {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handlePasswordToggle = () => {
+        setShowPassword((prevShowPassword) => !prevShowPassword);
+      };
+      
     otherProps = {
-        type: 'password',
+        type: showPassword ? "text" : 'password',
+        InputProps: {
+            endAdornment: (
+                <InputAdornment position="end">
+                    <IconButton onClick={handlePasswordToggle} edge="end" sx={{ color: 'primary.periwinkle' }}>
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                </InputAdornment>
+            ),
+        },
         ...otherProps
     }
     return <StringField otherProps={otherProps} {...props} />
 }
 
-export default field;
+export default Field;
