b3b2da2b83bd7e544bd01afe5479adf4f979deb6
Adarsh <adarsh.agarwal@spraxa.com>
2025-10-17 17:14:07 +0530

    Added localization

--- src/lib/components/Grid/index.js
+++ src/lib/components/Grid/index.js
@@ -607,7 +607,9 @@ const GridBase = memo(({
             showFullScreenLoader,
             history: navigate,
             baseFilters,
-            isElasticExport
+            isElasticExport,
+            tTranslate,
+            tOpts
         });
     };
     const openForm = (id, { mode } = {}) => {
